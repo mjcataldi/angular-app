c7f7f9939849f6867153e9b691eaab9e4f7895f3
Matt Cataldi <mcataldi@apics.org>
2018-12-21 15:22:15 -0600

    Practice with arrays and adding removing objects from the DOM.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,23 @@ import { FavoriteChangedEventargs } from "./favorite/favorite.component";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  viewMode = "map";
+  courses = [
+    { id: 1, name: "course1" },
+    { id: 2, name: "course2" },
+    { id: 3, name: "course3" },
+    { id: 4, name: "course4" }
+  ];
+
+  onAdd() {
+    let newCount = this.courses.length + 1;
+    this.courses.push({
+      id: newCount,
+      name: `course${newCount}`
+    });
+  }
+
+  onChange(course) {
+    let indexOfCourse = this.courses.indexOf(course);
+    this.courses.splice(indexOfCourse, 1);
+  }
 }
