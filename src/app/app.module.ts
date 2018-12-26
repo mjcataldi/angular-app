966675e8a5f0b5603ecbaeba429df9d9e07bb165
Matt Cataldi <mcataldi@apics.org>
2018-12-26 10:54:03 -0600

    Added Signup form components and other files. Setup in App.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from "./app.component";
 import { CoursesComponent } from "./courses.component";
 import { CourseComponent } from "./course/course.component";
 import { SummaryPipe } from "./summary.pipe";
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FavoriteComponent } from "./favorite/favorite.component";
+import { PanelComponent } from "./panel/panel.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent, SummaryPipe, FavoriteComponent, PanelComponent, ContactFormComponent],
+  declarations: [
+    AppComponent,
+    SignupFormComponent,
+    CoursesComponent,
+    CourseComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    PanelComponent,
+    ContactFormComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
